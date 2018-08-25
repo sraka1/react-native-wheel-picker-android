baa56e9a6a95766af9a20cd7c3cead9f4624b8da
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-08-24 20:46:39 -0500

    Cosmetic: example reformatting and update of package name

--- example/app/main.js
+++ example/app/main.js
@@ -1,23 +1,23 @@
 import MyPickers from "./my-pickers";
 
-import React, {Component} from "react";
-import {StyleSheet, View} from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  }
-});
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF"
+    }
+} );
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MyPickers/>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={ styles.container }>
+                <MyPickers/>
+            </View>
+        );
+    }
 }
--- example/app/my-pickers.js
+++ example/app/my-pickers.js
@@ -1,51 +1,51 @@
-import {DatePicker, TimePicker, WheelPicker} from 'react-native-wheel-picker-android'
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import { DatePicker, TimePicker, WheelPicker } from "@delightfulstudio/react-native-wheel-picker-android"
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-const wheelPickerData = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const wheelPickerData = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 const now = new Date();
 
 export default class MyPickers extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <WheelPicker
-          onItemSelected={(event) => this.onItemSelected(event)}
-          isCurved
-          data={wheelPickerData}
-          style={styles.wheelPicker}/>
-        <DatePicker
-          initDate={now.toISOString()}
-          onDateSelected={(date) => this.onDateSelected(date)}/>
-        <TimePicker
-          initDate={now.toISOString()}
-          onTimeSelected={(date) => this.onTimeSelected(date)}/>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={ styles.container }>
+                <WheelPicker
+                    onItemSelected={ ( event ) => this.onItemSelected( event ) }
+                    isCurved
+                    data={ wheelPickerData }
+                    style={ styles.wheelPicker }/>
+                <DatePicker
+                    initDate={ now.toISOString() }
+                    onDateSelected={ ( date ) => this.onDateSelected( date ) }/>
+                <TimePicker
+                    initDate={ now.toISOString() }
+                    onTimeSelected={ ( date ) => this.onTimeSelected( date ) }/>
+            </View>
+        );
+    }
 
-  onItemSelected(event) {
-    // do something
-  }
+    onItemSelected( event ) {
+        // do something
+    }
 
-  onDateSelected(date) {
-    // do something
-  }
+    onDateSelected( date ) {
+        // do something
+    }
 
-  onTimeSelected(date) {
-    // do something
-  }
+    onTimeSelected( date ) {
+        // do something
+    }
 
 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  wheelPicker: {
-    width: 200,
-    height: 150
-  }
-});
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1,
+        alignItems: "center"
+    },
+    wheelPicker: {
+        width: 200,
+        height: 150
+    }
+} );
